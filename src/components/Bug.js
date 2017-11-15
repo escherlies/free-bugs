e47aa2ff61ba2a8b27463086c4c4cee154b3db50
Enrico Scherlies <e.scherlies@me.com>
2017-11-15 13:20:51 +0100

    prep for crawling logic

--- src/components/Bug.js
+++ src/components/Bug.js
@@ -3,15 +3,39 @@ import React, { Component } from 'react';
 class Bug extends Component {
 
   state = {
-    tx: 0,
-    ty: 0,
-    ro: 0,
+    initalPosition: {
+      x: 0,
+      y: 0
+    },
+    translate: {
+      x: 0,
+      y: 0,
+    },
+    rotate: 0,
+  }
+
+  componentWillMount() {
+    this.setState({
+      initalPosition: {
+        x: Math.floor(Math.random() * this.props.parent.width),
+        y: Math.floor(Math.random() * this.props.parent.height)
+      }
+    })
   }
 
   componentDidMount() {
     const animate = (t) => {
-      this.setState({ tx: Math.sin(t/1000)*100})
-      requestAnimationFrame(animate)
+
+      
+      // TODO: crawling logic
+      this.setState({
+        translate: {
+          x: Math.sin(t / 1000) * 100,
+          y: Math.cos(t / 1000) * 100
+        },
+      })
+
+      if (t < 10000) requestAnimationFrame(animate)
     }
     requestAnimationFrame(animate)
   }
@@ -19,8 +43,11 @@ class Bug extends Component {
   render() {
 
     let sf = this.props.scalingFactor
+    let tx = this.state.translate.x
+    let ty = this.state.translate.y
+    let ro = this.state.rotate
 
-    const transform = `translate(${this.state.tx}px, ${this.state.ty}px) rotate(${this.state.ro}deg) scale(${sf},${sf})`
+    const transform = `translate(${tx}px, ${ty}px) rotate(${ro}deg) scale(${sf},${sf})`
 
     return (
       <div className="bug" style={{ transform }}>
--- src/components/Ground.js
+++ src/components/Ground.js
@@ -4,19 +4,32 @@ import Bug from './Bug'
 class Ground extends Component {
 
   state = {
-    scalingFactor: 0.1
+    scalingFactor: 0.1,
+    width: 100,
+    height: 100,
   }
 
+  componentDidMount() {
+    this.setState({
+      width: this.ground.clientWidth,
+      height: this.ground.clientHeight
+    })
+  }
 
   render() {
-  
+
+
     return (
-      <div className="">
-         <Bug 
+      <div className="ground" ref={e => this.ground = e}>
+        <Bug
           fileName={"01_A_Brentidae_Brentinae_Cephalobarus_macrocephalus.png"}
-          info={{ name: "macrocephalus"}}
+          info={{ name: "macrocephalus" }}
           scalingFactor={this.state.scalingFactor}
-         />
+          parent={{
+            width: this.state.width,
+            height: this.state.height
+          }}
+        />
       </div>
     );
   }
