08da335f0eacc747dafb9e80b7c241b50065a71f
Enrico Scherlies <e.scherlies@me.com>
2018-10-20 09:05:14 +0200

    Updates

---
+++ precache-manifest.814029b316d05b1a5a0e1377e45d36a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d560de58e8181d01321be33a174e433a",
+    "url": "/free-bugs/static/media/logo.d560de58.png"
+  },
+  {
+    "revision": "7c7a2da4c9e4c3d6f012752e196706e5",
+    "url": "/free-bugs/static/media/bg_pattern.7c7a2da4.png"
+  },
+  {
+    "revision": "6ac524210a1ed7e36f96",
+    "url": "/free-bugs/static/js/runtime~main.6ac52421.js"
+  },
+  {
+    "revision": "6bb632d4943ee6c50ab4",
+    "url": "/free-bugs/static/js/main.6bb632d4.chunk.js"
+  },
+  {
+    "revision": "6392e67b499410f156d4",
+    "url": "/free-bugs/static/js/1.6392e67b.chunk.js"
+  },
+  {
+    "revision": "6bb632d4943ee6c50ab4",
+    "url": "/free-bugs/static/css/main.d3f6b647.chunk.css"
+  },
+  {
+    "revision": "bd22f23104e3b9d67ea1c9fbef328b66",
+    "url": "/free-bugs/index.html"
+  }
+];
